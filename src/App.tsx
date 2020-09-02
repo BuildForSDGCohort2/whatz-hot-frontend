cce0413752fc86d1f3f788bb5e5970490adfe35a
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2020-09-02 15:37:05 +0100

    chore: userlogin and usersignup setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import header from './container/Header';
 import './App.css';
 import Error404 from './container/Error404';
+import { createGlobalStyle } from 'styled-components';
 
 const App = () => {
   return (
     <Router>
+      <GlobalStyle />
       <Switch>
         <Route exact path='/' component={header} />
         <Route path='*' component={Error404} />
@@ -15,4 +17,15 @@ const App = () => {
   );
 };
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Playfair Display', Arial, Helvetica, sans-serif;
+  }
+  body {
+    font-family: 'Playfair Display', Arial, Helvetica, sans-serif;
+  }
+`;
 export default App;
--- src/container/Header.tsx
+++ src/container/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Card, Button, Elevation, H1 } from '@blueprintjs/core';
+import { Card, Button, Elevation, H6 } from '@blueprintjs/core';
 
 const Header = () => {
   return (
     <div className='App'>
-      <Card interactive={true} elevation={Elevation.TWO}>
-        <H1>Welcome to Whatz-hot</H1>
-        <Button>Welcome to Whatz-hot</Button>
+      <Card
+        interactive={true}
+        elevation={Elevation.TWO}
+        style={{ backgroundColor: '#3DCC91' }}
+      >
+        <H6>Welcome to Whatz-hot</H6>
+        <Button intent={'success'}>Welcome to Whatz-hot</Button>
       </Card>
     </div>
   );
