d7e109310b6343d282627ceefb5d808d23d0b44f
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2020-09-05 22:49:31 +0100

    feat: user sign up page

--- src/utils/Uiconfig.ts
+++ src/utils/Uiconfig.ts
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: 'AIzaSyA5heY93tcjP8jIZG9Xdz-OgnTRmzUvH3I',
+  authDomain: 'whatzhot-d4be8.firebaseapp.com',
+  databaseURL: 'https://whatzhot-d4be8.firebaseio.com',
+  projectId: 'whatzhot-d4be8',
+  storageBucket: 'whatzhot-d4be8.appspot.com',
+  messagingSenderId: '586486734887',
+  appId: '1:586486734887:web:bc62a3d980f3c13e61ee02'
 };
 
 if (!firebase.apps.length) {
