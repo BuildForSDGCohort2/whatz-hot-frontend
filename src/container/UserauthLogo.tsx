b6f0f1da8fb5ba86fa1800fc1d1990d3341356cc
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2020-09-08 10:09:35 +0100

    feat: user sign up page

--- src/container/UserauthLogo.tsx
+++ src/container/UserauthLogo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import signimg from '../images/signup_illustration.png';
+import signimg from '../images/logo.png';
 
 const UserauthLogo = () => {
   return (
@@ -14,15 +14,29 @@ const UserauthLogo = () => {
 
 const SideWrapper = styled.div`
   width: 100%;
-  max-width: 300px;
+  max-width: 550px;
   height: 400px;
   margin-top: 150px;
+  padding-left: 35px;
   @media (max-width: 500px) {
     display: none;
+  }
+  @media (max-width: 675px) {
+    width: 100%;
+    max-width: 300px;
+    height: 100%;
+    max-height:300px;
+    margin-top: 250px;
+    padding:left: 10px;
+    padding-right: 5px;
+  }
+  @media(max-width: 843px){
+    padding-right: 5px;
   }
 `;
 const Img = styled.img`
   max-width: 100%;
   height: auto;
+  object-fit: cover;
 `;
 export default UserauthLogo;
