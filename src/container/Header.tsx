ec8b89249c81e042cc2f6055fb3265c5fa5ea149
Dave <callremz@gmail.com>
2020-09-23 15:20:29 +0100

    feat: add temporary links to signup and login

--- src/container/Header.tsx
+++ src/container/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Button, Elevation, H6 } from '@blueprintjs/core';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -11,6 +12,13 @@ const Header = () => {
       >
         <H6>Welcome to Whatz-hot</H6>
         <Button intent={'success'}>Welcome to Whatz-hot</Button>
+        {/* This is temporary please remove */}
+        <div id="temp-div">
+          <Link to="/signup">Sign up</Link>
+          <br/>
+          <Link to="/login">Login</Link>
+        </div>
+        {/* Ends here */}
       </Card>
     </div>
   );
